aed598f9d09a0fa6c4a9504368796cd6e5661374
Jude_Sun <Jude_Sun@infosys.com>
2018-03-27 15:09:14 +0800

    local commit

--- src/main/java/com/jude/gitsample/App.java
+++ src/main/java/com/jude/gitsample/App.java
@@ -11,5 +11,6 @@ public class App
         System.out.println( "Hello World!" );
         System.out.println("hhaja...");
         System.out.println("upgrade....");
+        System.out.println("upgrade 02");
     }
 }
